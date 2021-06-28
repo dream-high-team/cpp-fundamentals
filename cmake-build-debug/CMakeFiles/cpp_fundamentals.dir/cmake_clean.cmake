ae690491d8ef2c75f27db8964cd487086a4e46e2
Sandy Andryanto <sandyandryanto@gmail.com>
2021-06-28 12:00:27 +0700

    init

---
+++ cmake-build-debug/CMakeFiles/cpp_fundamentals.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_fundamentals.dir/main.cpp.obj"
+  "cpp_fundamentals.pdb"
+  "cpp_fundamentals.exe"
+  "cpp_fundamentals.exe.manifest"
+  "libcpp_fundamentals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_fundamentals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
